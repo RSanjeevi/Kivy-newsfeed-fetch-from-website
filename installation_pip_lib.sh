0f54beb5605c01245e7ad8d60ec3650e131df89a
RSanjeevi <35596593+RSanjeevi@users.noreply.github.com>
2019-03-23 12:45:05 +0530

    Create installation_pip_lib.sh

---
+++ installation_pip_lib.sh
@@ -0,0 +1,8 @@
+pip install --upgrade pip wheel setuptools
+pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+pip install kivy.deps.gstreamer
+pip install kivy.deps.angle
+pip install kivy
+pip install kivy_examples
+pip install bs4
+pip install urllib
